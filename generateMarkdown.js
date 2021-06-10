ffb5e63d9e344fda822d9f620afdc60a4c121f1d
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-06-09 20:58:08 -0700

    added questions

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,21 +1,55 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Descirption
+  ${data.description}
+
+  ## Table of Contents
+  ${data.tofcont}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage} 
+
+  ## License
+  ${data.license} 
+
+  ## Contributing 
+  ${data.contributing}
+
+  ## Test
+  ${data.test}
+
+  ##Questions
+
 
 `;
 }
 
 module.exports = generateMarkdown;
 //the generateMarkdown needs to be required in index.js
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+// function renderLicenseBadge(license) {
+//   //obj constuction notation?
+//   //if statement
+//   //else return
+// }
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   //if
+//   //else return empty string?
+// }
+//should this go inside?
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+// function renderLicenseSection(license) {
+// //
+// }
--- index.js
+++ index.js
@@ -1,20 +1,89 @@
 // TODO: Include packages needed for this application
 //here we add the inquierer npm and the fs
-//const inquirer = require('inquirer');
-//const fs = require('fs');
-//const genMarkdown = require('generateMarkdown');//doublecheck
+
+const inquirer = require('inquirer');
+// const fs = require('fs');
+
+// const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 //README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation,
 //Usage, License, Contributing, Tests, and Questions
 
-const questions = [];
+//array for questions 
+//const questions = () => {
+    //return inq
+//} 
+const questions = () => {
+    inquirer
+        .prompt([
+            //inquirer then prompt
+            {
+            type: 'input',
+            message: 'What would you like to name your project?',
+            name: 'title',
+        
+            },
+            {
+            type: 'input',
+            message: 'Describe the project your working on:',
+            name: 'description',
+            },
+            {
+            //its a question so type: '',
+            message: 'Would you like a Table of Contents? ',
+            name: 'tofcont',
+            },
+            {
+            type: 'input',
+            message: 'Enter installation steps for your project: ',
+            name: 'installation',
+            },
+            {
+            type: 'input',
+            message: 'Enter how to use your project?',
+            name: 'usage',
+            },
+            {
+            type: 'input',
+            message: 'Enter the license:',
+            name: 'license',
+            },
+            {
+            type: 'input',
+            message: 'Is there any contributors? ',
+            name: 'contributing',
+            },
+            {
+            type: 'input',
+            message: 'How would you test your project? ',
+            name: 'test',
+            },
+            {
+            type: 'input',
+            message: ' ',
+            name: 'questions',
+            },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        ])
+}
+    
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+questions()
+//      .then((results)) => {
+//           return generateMarkdown(results);
+//      })
+//      .then(filename => {
+//          return writteFile(filename)
+// })
+
+//  TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     //generatemarkdown passed here?
+// }
